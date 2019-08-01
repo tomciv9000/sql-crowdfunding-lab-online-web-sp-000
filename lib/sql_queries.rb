ad0a77eed34921baab674812021c796a88dc3c3c
Tom White <eugene.white@gmail.com>
2019-08-01 03:35:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,12 +32,12 @@ HAVING amount_over_goal >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT name, amount
+"SELECT name, SUM(amount) AS total
 FROM users
 INNER JOIN pledges
 	ON pledges.user_id = users.id
 GROUP BY name
-ORDER BY amount, name;"
+ORDER BY total, name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
