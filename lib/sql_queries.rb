24ce87caef84d9e3ef9ad1d37dd15cacdbe2c857
Tom White <eugene.white@gmail.com>
2019-08-01 03:38:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,7 +41,11 @@ ORDER BY total, name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT category, amount
+FROM projects
+INNER JOIN pledges
+	ON pledges.project_id = projects.id
+WHERE category = "music";"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
