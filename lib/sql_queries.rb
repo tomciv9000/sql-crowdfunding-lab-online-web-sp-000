8ee53650764bb0976575a27e3c0225d05caf2af3
Tom White <eugene.white@gmail.com>
2019-08-01 03:43:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,5 +49,9 @@ WHERE category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT category, SUM(amount) as total_pledged
+FROM projects
+INNER JOIN pledges
+	ON pledges.project_id = projects.id
+WHERE category = 'books';"
 end
